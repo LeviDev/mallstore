32cb9847ea710d09401f58d00d593bc9e985d02b
Levi Xie <mytest314@163.com>
2018-08-24 17:51:40 +0800

    add login menu

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <van-nav-bar title="用户注册" left-text="返回" left-arrow @click-left="goBack"></van-nav-bar>
-        <div class="register-panel">
+        <van-nav-bar title="用户登录" left-text="返回" left-arrow @click-left="goBack"></van-nav-bar>
+        <div class="login-panel">
             <van-field v-model="username" label="用户名" icon="clear" placeholder="请输入用户名" required @click-icon="username=''" :error-message="nameErrorMessage">
             </van-field>
             <van-field v-model="password" type="password" label="密码" placeholder="请输入密码" required :error-message="passwordErrorMessage">
             </van-field>
-            <div class="register-btn">
-                <van-button type="primary" size="large" @click="registerAction" :loading="loading">注册</van-button>
+            <div class="login-btn">
+                <van-button type="primary" size="large" @click="loginAction" :loading="loading">登录</van-button>
             </div>
         </div>
     </div>
@@ -32,7 +32,7 @@ export default {
         goBack() {
             this.$router.go(-1)
         },
-        checkRegisterForm() {
+        checkLoginForm() {
             let isOK = true
             if (this.username.length < 4) {
                 isOK = false
@@ -48,15 +48,15 @@ export default {
             }
             return isOK
         },
-        registerAction() {
-            if (this.checkRegisterForm()) {
-                this.axiosRegister()
+        loginAction() {
+            if (this.checkLoginForm()) {
+                this.axiosLogin()
             }
         },
-        axiosRegister() {
+        axiosLogin() {
             this.loading = true
             axios({
-                url: url.registerUser,
+                url: url.loginUser,
                 method: 'post',
                 data: {
                     userName: this.username,
@@ -69,13 +69,13 @@ export default {
                         this.$router.push('/')
                     } else {
                         console.log(response.data.message)
-                        Toast.fail('注册失败！')
+                        Toast.fail('登录失败')
                         this.loading = false
                     }
                 })
                 .catch(error => {
                     console.log(error)
-                    Toast.fail('注册失败！')
+                    Toast.fail('登录失败')
                     this.loading = false
                 })
         }
@@ -84,14 +84,14 @@ export default {
 </script>
 
 <style scoped>
-.register-panel {
+.login-panel {
     width: 100%;
     border-radius: 5px;
     margin: 20px auto;
     padding-bottom: 10px;
 }
 
-.register-btn {
+.login-btn {
     margin-top: 10px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 
 import ShoppingMall from '@/components/pages/ShoppingMall'
 import Register from '@/components/pages/Register'
+import Login from '@/components/pages/Login'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/register',
       name: 'Register',
       component: Register
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
